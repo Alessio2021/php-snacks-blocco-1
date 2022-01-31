60e2ad18e197c9ae95215f15bd65e067d1f20b9d
Alessio2021 <alessiobut1911@gmail.com>
2022-01-31 15:43:47 +0100

    snack 4

--- snack4.php
+++ snack4.php
@@ -1,2 +1,16 @@
 <!-- ## Snack 4
 Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+
+<?php
+
+$array = [];
+
+while (count($array) < 15) {
+    $random = rand(1, 100);
+    if (!in_array($random, $array)) {
+        $array[] = $random;
+    }
+}
+var_dump($array);
+
+?>
